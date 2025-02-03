4d831465ba940d53f1f4f30b4a3f203bbdee8f61
ggang89 <ggang89@gmail.com>
2025-02-03 23:19:56 +0900

    navbar 생성 + css

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,15 @@
 
 import './App.css'
+import Nav from './components/Nav'
 
 function App() {
-  
+  return (
+    <>
+      <div className='app'>
+        <Nav/>
+    </div>
+    </>
+  )
 }
 
 export default App

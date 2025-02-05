88d9e5891caf644515de1fee0cb4ee03beaa0d42
ggang89 <ggang89@gmail.com>
2025-02-06 00:27:32 +0900

    [add] styled components , iframe을 이용한 영화 예고편 재생하기

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -1,11 +1,14 @@
 import axios from "../api/axios";
 import { useEffect, useState } from "react";
 import requests from "../api/requsts";
-import "./Banner.css"
+import "./Banner.css";
+import styled from "styled-components";
 
 export default function Banner() {
+  
   const [movie, setMovie] = useState([]);
-
+  const [isClicked, setIsClicked] = useState(false);
+console.log("movie", movie);
   useEffect(() => {
     fetchData();
   }, []);
@@ -13,50 +16,102 @@ export default function Banner() {
   const fetchData = async () => {
     // 현재 상영중인 영화 정보를 가져오기(여러 영화)
     const request = await axios.get(requests.fetchNowPlaying);
-    
+
     // 여러 영화 중 하나의 Id 가져오기
-    const movieId = request.data.results[
-      Math.floor(Math.random()*request.data.results.length)
-    ].id;
+    const movieId =
+      request.data.results[
+        Math.floor(Math.random() * request.data.results.length)
+      ].id;
 
     // 선택된 id 영화이 상세한 정보 가져오기
-    const {data:movieDetail} = await axios.get(`movie/${movieId}`, {
-      params: { append_to_reponse: 'videos' },
+    const { data: movieDetail } = await axios.get(`movie/${movieId}`, {
+      params: { append_to_response: "videos" },
     });
     setMovie(movieDetail);
   };
-const truncate = (str, n) => {
-  return str?.length > n ? str.substr(0, n - 1) + "..." : str;
-};
-  return (
-    <header className="banner"
-      style={{
-        backgroundImage: `url("https://image.tmdb.org/t/p/original/${movie.backdrop_path}")`,
-        backgroundPosition: "top center",
-        backgroundSize:"cover",
-    }}>
-      <div className="banner_contents">
-        {/* title */}
-        <h1 className="banner_title">
-          {movie?.title||movie?.name||movie?.original_name}
-        </h1>
-        <div className="banner_buttons">
-          <button className="banner_button play">
-            Play
-          </button>
-          <button className="banner_button info">
-           More Information
-          </button>
+  const truncate = (str, n) => {
+    return str?.length > n ? str.substr(0, n - 1) + "..." : str;
+  };
+  if (!isClicked) {
+    return (
+      <header
+        className="banner"
+        style={{
+          backgroundImage: `url("https://image.tmdb.org/t/p/original/${movie.backdrop_path}")`,
+          backgroundPosition: "top center",
+          backgroundSize: "cover",
+        }}
+      >
+        <div className="banner_contents">
+          {/* title */}
+          <h1 className="banner_title">
+            {movie?.title || movie?.name || movie?.original_name}
+          </h1>
+          <div className="banner_buttons">
+            <button
+              className="banner_button play"
+              onClick={() => {
+                setIsClicked(true);
+              }}
+            >
+              Play
+            </button>
+            <button className="banner_button info">More Information</button>
+          </div>
+          {/* {DIV >2 BUTTONS} */}
+          <h1 className="banner_description">
+            {truncate(movie?.overview, 150)}
+          </h1>
+          {/* Description */}
         </div>
-        {/* {DIV >2 BUTTONS} */}
-        <h1 className="banner_description">
-          {truncate(movie?.overview,100)}
-        </h1>
-        {/* Description */}
-      </div>
-      <div className="banner_fadeBottom"/>
-
-  </header>
-  
-  );
+        <div className="banner_fadeBottom" />
+      </header>
+    );
+  } else {
+    return (
+      <Container>
+        <HomeContainer>
+          <Iframe
+            width="640"
+            height="360"
+            src={`https://www.youtube.com/embed/${movie.videos.results[0].key}?controls=0&autoplay=1&loop=1&mute=1&playlist=${movie.videos.results[0].key}`}
+            title="YouTube video player"
+            frameborder="0"
+            allow=" autoplay; fullscreen"
+            referrerpolicy="strict-origin-when-cross-origin"
+            allowfullscreen
+          ></Iframe>
+        </HomeContainer>
+      </Container>
+    );
+  }
 }
+
+const Container = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  width: 100%;
+  height: 100vh;
+`;
+const HomeContainer = styled.div`
+  width: 100%;
+  height: 100%;
+`;
+const Iframe=styled.iframe`
+  width: 100%;
+  height: 100%;
+  z-index: -1;
+  opacity: 0.65;
+  border:none;
+
+  &::after{
+    content: "";
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+  }
+`

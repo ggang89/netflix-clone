4a21562a04c2c6cd5f286f5bd7dc72d82bc3b721
ggang89 <ggang89@gmail.com>
2025-02-23 00:31:06 +0900

    env파일  생성하고 gitignore에 추가

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -3,6 +3,7 @@ import axios from "axios";
 const instance = axios.create({
   baseURL: "https://api.themoviedb.org/3",
   params: {
+    // vite에서 env 파일사용시 import.meta.env.VITE_변수명으로 사용
     api_key: import.meta.env.VITE_MOVIE_DB_API_KEY,
     language: "ko-KR",
   },
